c0c95bfe554402f905b4797fb1fd0b268f622e37
ageneth <agenethb@gmailcom>
2025-10-06 08:24:04 +0800

    Updated project files for Semi_Act1

--- App.js
+++ App.js
@@ -1,15 +1,24 @@
 import ChatScreen from './ChatScreen';
 import React from "react";
+import { Platform, KeyboardAvoidingView, Image } from "react-native";
 
 import CommentSection from "./CommentSection";
 
 export default function App() {
   return  (
-   <>
+   <KeyboardAvoidingView
+   style ={{flex: 1}}
+   behavior={Platform.OS ==="ios" ? "padding" : "height"}
+   >
+     <Image
+     source={require ('./assets/expogopik.jpeg')}
+     
+     style={{width:450, height:350}}
+     />
 <ChatScreen />
 <CommentSection />
 
-</>
-)
+</KeyboardAvoidingView>
+);
   
 }
